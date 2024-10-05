6048bdff28bfeffd50418030d865532ccbdd1037
sahand-motallebi <sahandmotallebi@outlook.com>
2024-10-06 01:27:00 +0330

    some change in youtube downloader

--- core/main.py
+++ core/main.py
@@ -3,7 +3,9 @@ import my_data
 from datetime import datetime
 import os 
 from gtts import gTTS
-import requests
+from pytubefix import YouTube
+from moviepy.editor import VideoFileClip, AudioFileClip
+
 
 API_TOKEN = os.environ.get('API_TOKEN')
 
@@ -47,41 +49,71 @@ def get_last_name(message, first_name):
     bot.reply_to(message, "Your information has been saved successfully!")
     my_data.close_connection(conn)
 
-# هندلر دانلود فایل‌های MP3 و MP4
-def download_file(url):
-    local_filename = url.split('/')[-1]
-    
-    if local_filename.endswith('.mp3') or local_filename.endswith('.mp4'):
-        file_path = os.path.join(DOWNLOAD_DIR, local_filename)
 
-        try:
-            with requests.get(url, stream=True) as r:
-                r.raise_for_status()
-                with open(file_path, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        f.write(chunk)
-            return file_path
-        except requests.exceptions.RequestException as e:
-            return None
-    else:
-        return None
 
-@bot.message_handler(commands=['download'])
-def ask_for_url(message):
-    bot.reply_to(message, "Please send a valid MP3 or MP4 URL.")
 
-@bot.message_handler(func=lambda message: message.text.startswith('http'))
-def handle_message(message):
+@bot.message_handler(commands=['youtube'])
+def send_welcome(message):
+    bot.reply_to(message, "welcome to youtube downloader bot")
+
+@bot.message_handler(func= lambda message:True ,content_types=['text'])
+def url_handler(message):
     url = message.text
-    bot.send_message(message.chat.id, "File is downloading...")
-    file_path = download_file(url)
+    try:
+        yt = YouTube(url)
+        bot.reply_to(message, "select the quality", reply_markup=quality_markup(yt))
+    except Exception as e:
+        print(f"Error:{e}")
+        bot.reply_to(message, "invalid youtube")
+        
 
-    if file_path:
-        with open(file_path, 'rb') as f:
-            bot.send_document(chat_id=message.chat.id, document=f)
-        os.remove(file_path)
-    else:
-        bot.reply_to(message, "Invalid URL or file format. Please send a valid MP3 or MP4 URL.")
+def quality_markup(yt):                
+    markup = telebot.types.InlineKeyboardMarkup()
+    streams = yt.streams.filter(mime_type = "video/mp4")
+    
+    for stream in streams:
+        resolution = stream.resolution
+        button_text = f"{resolution} - {stream.mime_type}"
+        callback_data = f"{stream.itag}|{yt.watch_url}"
+        button = telebot.types.InlineKeyboardButton(button_text, callback_data=callback_data)
+        markup.add(button)
+    return markup    
+
+
+@bot.callback_query_handler(func= lambda call:True)
+def handle_callback(call):
+    itag, url = call.data.split('|')
+    yt = YouTube(url)
+    stream = yt.streams.get_by_itag(itag)
+    video_filename = "video.mp4"
+    audio_filename = "audio.mp4"
+    final_filename = "final_video.mp4"
+    
+    stream.download(filename=video_filename)
+    try:
+        if not stream.is_progressive:
+            audio_stream = yt.streams.filter(only_audio=True).first()
+            audio_stream.download(filename=audio_filename)
+            video_clip = VideoFileClip(video_filename)
+            audio_clip = AudioFileClip(audio_filename)
+            
+            final_clip = video_clip.set_audio(audio_clip)
+            final_clip.write_videofile(final_filename, codec='libx264')
+            
+            with open(final_filename, 'rb') as video:
+                bot.send_video(call.message.chat.id, video)
+                
+            os.remove(video_filename)
+            os.remove(audio_filename)
+            os.remove(final_filename)
+        else:
+            with open(video_filename, 'rb') as video:
+                bot.send_video(call.message.chat.id, video)
+            os.remove(video_filename)
+    except Exception as e:
+        print(f"error sending file: {e}")
+        bot.reply_to(call.message, "failed top send")
+
 
 @bot.message_handler(commands=['voice'])
 def send_voice_prompt(message):
--- core/video.py
+++ core/video.py
@@ -1,71 +1,71 @@
-# import telebot
-# from pytubefix import YouTube
-# import os 
-# from moviepy.editor import VideoFileClip, AudioFileClip
+import telebot
+from pytubefix import YouTube
+import os 
+from moviepy.editor import VideoFileClip, AudioFileClip
 
-# API_TOKEN = os.environ.get('API_TOKEN')
-# bot = telebot.TeleBot(API_TOKEN)
+API_TOKEN = os.environ.get('API_TOKEN')
+bot = telebot.TeleBot(API_TOKEN)
 
-# @bot.message_handler(commands=['youtube'])
-# def send_welcome(message):
-#     bot.reply_to(message, "welcome to youtube downloader bot")
+@bot.message_handler(commands=['youtube'])
+def send_welcome(message):
+    bot.reply_to(message, "welcome to youtube downloader bot")
 
-# @bot.message_handler(func= lambda message:True ,content_types=['text'])
-# def url_handler(message):
-#     url = message.text
-#     try:
-#         yt = YouTube(url)
-#         bot.reply_to(message, "select the quality", reply_markup=quality_markup(yt))
-#     except Exception as e:
-#         print(f"Error:{e}")
-#         bot.reply_to(message, "invalid youtube")
+@bot.message_handler(func= lambda message:True ,content_types=['text'])
+def url_handler(message):
+    url = message.text
+    try:
+        yt = YouTube(url)
+        bot.reply_to(message, "select the quality", reply_markup=quality_markup(yt))
+    except Exception as e:
+        print(f"Error:{e}")
+        bot.reply_to(message, "invalid youtube")
         
 
-# def quality_markup(yt):                
-#     markup = telebot.types.InlineKeyboardMarkup()
-#     streams = yt.streams.filter(mime_type = "video/mp4")
+def quality_markup(yt):                
+    markup = telebot.types.InlineKeyboardMarkup()
+    streams = yt.streams.filter(mime_type = "video/mp4")
     
-#     for stream in streams:
-#         resolution = stream.resolution
-#         button_text = f"{resolution} - {stream.mime_type}"
-#         callback_data = f"{stream.itag}|{yt.watch_url}"
-#         button = telebot.types.InlineKeyboardButton(button_text, callback_data=callback_data)
-#         markup.add(button)
-#     return markup    
+    for stream in streams:
+        resolution = stream.resolution
+        button_text = f"{resolution} - {stream.mime_type}"
+        callback_data = f"{stream.itag}|{yt.watch_url}"
+        button = telebot.types.InlineKeyboardButton(button_text, callback_data=callback_data)
+        markup.add(button)
+    return markup    
 
 
-# @bot.callback_query_handler(func= lambda call:True)
-# def handle_callback(call):
-#     itag, url = call.data.split('|')
-#     yt = YouTube(url)
-#     stream = yt.streams.get_by_itag(itag)
-#     video_filename = "video.mp4"
-#     audio_filename = "audio.mp4"
-#     final_filename = "final_video.mp4"
+@bot.callback_query_handler(func= lambda call:True)
+def handle_callback(call):
+    itag, url = call.data.split('|')
+    yt = YouTube(url)
+    stream = yt.streams.get_by_itag(itag)
+    video_filename = "video.mp4"
+    audio_filename = "audio.mp4"
+    final_filename = "final_video.mp4"
     
-#     stream.download(filename=video_filename)
-#     try:
-#         if not stream.is_progressive:
-#             audio_stream = yt.streams.filter(only_audio=True).first()
-#             audio_stream.download(filename=audio_filename)
-#             video_clip = VideoFileClip(video_filename)
-#             audio_clip = AudioFileClip(audio_filename)
+    stream.download(filename=video_filename)
+    try:
+        if not stream.is_progressive:
+            audio_stream = yt.streams.filter(only_audio=True).first()
+            audio_stream.download(filename=audio_filename)
+            video_clip = VideoFileClip(video_filename)
+            audio_clip = AudioFileClip(audio_filename)
             
-#             final_clip = video_clip.set_audio(audio_clip)
-#             final_clip.write_videofile(final_filename, codec='libx264')
+            final_clip = video_clip.set_audio(audio_clip)
+            final_clip.write_videofile(final_filename, codec='libx264')
             
-#             with open(final_filename, 'rb') as video:
-#                 bot.send_video(call.message.chat.id, video)
+            with open(final_filename, 'rb') as video:
+                bot.send_video(call.message.chat.id, video)
                 
-#             os.remove(video_filename)
-#             os.remove(audio_filename)
-#             os.remove(final_filename)
-#         else:
-#             with open(video_filename, 'rb') as video:
-#                 bot.send_video(call.message.chat.id, video)
-#             os.remove(video_filename)
-#     except Exception as e:
-#         print(f"error sending file: {e}")
-#         bot.reply_to(call.message, "failed top send")
+            os.remove(video_filename)
+            os.remove(audio_filename)
+            os.remove(final_filename)
+        else:
+            with open(video_filename, 'rb') as video:
+                bot.send_video(call.message.chat.id, video)
+            os.remove(video_filename)
+    except Exception as e:
+        print(f"error sending file: {e}")
+        bot.reply_to(call.message, "failed top send")
 
-# bot.infinity_polling()                                      
+bot.infinity_polling()                                      
